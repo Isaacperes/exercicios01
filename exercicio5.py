ff0ddec80653419fcc55d64e9d7c41ef843a20e2
Peres_05 <isaacperes05@gmail.com>
2024-07-10 13:51:01 -0300

    2024.07.10

---
+++ exercicio5.py
@@ -0,0 +1,4 @@
+var_inteiro = int(input("Digite um número inteiro: "))
+var_decimal = float(input("Digite um número decimal usando '.': "))
+var_palavra = str(input("Digite uma palavra: "))
+print(f"Você digitou o inteiro {var_inteiro}, o decimal {var_decimal} e a palavra {var_palavra}.")
---
+++ exercicio6.py
@@ -0,0 +1,5 @@
+var_numero1 = float(input("Digite um número: "))
+var_numero2 = float(input("Digite um número: "))
+var_numero3 = float(input("Digite um número: "))
+var_media = (var_numero1 + var_numero2 + var_numero3) / 3
+print(f"A média é {var_media:.2f}")
